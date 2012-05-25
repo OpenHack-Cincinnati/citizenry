cdf12adf94ce8af6872461ae919b23d5933f5e4a
Chris Moore <cdmwebs@gmail.com>
2012-05-25 13:20:38 -0400

    allow settings to be loaded from ENV

--- config/initializers/_load_settings.rb
+++ config/initializers/_load_settings.rb
@@ -1,5 +1,7 @@
 # Load application settings from config/settings.yml
-settings_yml = HashWithIndifferentAccess.new(YAML.load_file(Rails.root.join('config', 'settings.yml')))
+file = Rails.root.join('config', 'settings.yml')
+settings = ERB.new(File.read(file)).result
+settings_yml = HashWithIndifferentAccess.new(YAML.load(settings))
 
 merged_settings = settings_yml['common']
 merged_settings.deep_merge!(settings_yml[Rails.env]) if settings_yml.has_key?(Rails.env)
