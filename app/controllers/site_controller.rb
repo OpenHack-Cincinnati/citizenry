dd48fc327280a6f48c7c6c6f5466028606927b1c
Chris Moore <cdmwebs@gmail.com>
2012-05-28 21:56:26 -0400

    load count from db

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,10 +4,7 @@ class SiteController < ApplicationController
   def index
     page_title false
 
-    events_count_path = 'tmp/calagator_events_count'
-    if File.exists?(events_count_path)
-      @events_count = File.read(events_count_path).to_i
-    end
+    @events_count = Event.event_count
   end
 
   def search
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ActiveRecord::Base
+
+  def self.event_count
+    self.first ? self.first.count : nil
+  end
 end
